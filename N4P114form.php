dcdd49c84de24bc7c46769bba0b029f566e78916
daw2-aspas21 <71894719+daw2-aspas21@users.noreply.github.com>
2020-11-23 17:14:56 +0100

    Add files via upload

---
+++ N4P114form.php
@@ -0,0 +1,15 @@
+<html>
+ <head>
+  <title>Calculadora</title>
+</head>
+<body>
+<form method="get" action="N4P115form.php">
+		<input type="text" name="operando1">
+		<select name="operador">
+			<option value="+">+</option>
+		</select>
+	    <input type="text" name="operando1">
+		<input type="submit" value="enviar">
+</form>
+</body>
+</html>
